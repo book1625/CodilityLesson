de417dc1625aee10c86b456cf4222571aa581b01
book1625 <book1625@gmail.com>
2017-09-18 18:23:00 +0800

    Initial check-in of module CodilityLesson

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodilityLesson
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
