6644718e75841a8d93f68b3294e28010fda67304
book1625 <book1625@gmail.com>
2017-09-18 18:35:36 +0800

    Try my first commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace CodilityLesson
+
+namespace Codility
 {
+    public static class ExtendClass
+    {
+        public static string ToArrayString(this int[] inData)
+        {
+            return string.Format("[ {0} ]", string.Join(",", inData));
+        }
+    }
+
     class MainClass
     {
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DynamicProgramming.Test();
+
+            var runCode = new NumberOfDiscIntersections();
+
+            //11
+            Console.WriteLine(runCode.Solution(new int[] { 1, 5, 2, 1, 4, 0 }));
+
+            Console.WriteLine(runCode.Solution(new int[] { 1, 2147483647, 0 }));         
+                     
         }
     }
 }
